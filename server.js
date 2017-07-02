b4b3a09e52703211347b3a03e53545849374ae03
Steven Jirjis <sjirjis@tessituranetwork.com>
2017-07-01 23:11:02 -0700

    package.json changes for heroku

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', api);
 
 // Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 /**
